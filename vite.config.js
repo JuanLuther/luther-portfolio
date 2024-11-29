4f0b537574bac72e19cf2cf4e010e3bed954fcd3
JuanLuther <juanlutherhernando@gmail.com>
2024-11-29 11:15:00 +0800

    cool

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio",
   build: {
-    outDir: "./root", // Set the output directory to the project root
-    emptyOutDir: false, // Prevent Vite from deleting other files in the root
+    // outDir: "./root", // Set the output directory to the project root
+    // emptyOutDir: false, // Prevent Vite from deleting other files in the root
+    rollupOptions: {
+      output: {
+        // This ensures the 404.html is correctly handled
+        entryFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash].[ext]",
+      },
+    },
   },
 });
